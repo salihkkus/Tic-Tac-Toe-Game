88927fb470f7f208414301b0410981dbc4b0f3a3
salihkarakus <sskarakussalih77@gmail.com>
2025-04-18 15:04:54 +0300

    scripts and scene updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 public static GameManager Instance {get; private set;}
+public List<TileController> ListTileController => listTileController;
+
+[SerializeField] private List<TileController> listTileController;
 
 public int Turn{get; set;}
 
@@ -12,5 +16,31 @@ public int Turn{get; set;}
     {
         Instance = this;
     }
+
+  public (bool, TileState) HasWinner()
+	{
+		foreach (var tile in listTileController)
+		{
+			if (tile == null) continue;
+
+			foreach (var direction in Enum.GetValues(typeof(Direction)))
+			{
+				var next = tile.GetNextTile((Direction) direction);
+				if (!next) continue;
+
+				if (next.MyState != tile.MyState) continue;
+
+				var lastTile = next.GetNextTile((Direction) direction);
+				if (!lastTile) continue;
+
+				if (lastTile.MyState != tile.MyState) continue;
+				
+				return (true, tile.MyState);
+			}
+		}
+
+		return (false, TileState.None);
+}
+
 
 }
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
 public class TileController : MonoBehaviour, IPointerDownHandler
 {
@@ -15,11 +16,19 @@ public class TileController : MonoBehaviour, IPointerDownHandler
 
   public void  OnPointerDown(PointerEventData eventData)
   {
-    Debug.Log("click");
+    if(MyState != TileState.None)
+    return;
 
     var state = GameManager.Instance.Turn % 2 == 0 ? TileState.X : TileState.O;
     SetState(state);
     GameManager.Instance.Turn++;
+
+     var result = GameManager.Instance.HasWinner();
+    if(result.Item1)
+    {
+      Debug.Log($"Winner => {result.Item2}");
+    }
+
   }
 
    public void SetState(TileState state)
@@ -27,11 +36,58 @@ public class TileController : MonoBehaviour, IPointerDownHandler
     MyState = state;
     mySpriteRenderer.color = state == TileState.X ? xColor : oColor;
   }
+
+  public TileController GetNextTile(Direction direction)
+	{
+		var nextTileCoordinate = coordinate;
+		switch (direction)
+		{
+			case Direction.Up:
+				nextTileCoordinate.y++;
+				break;
+			case Direction.UpRight:
+				nextTileCoordinate.y++;
+				nextTileCoordinate.x++;
+				break;
+			case Direction.Right:
+				nextTileCoordinate.x++;
+				break;
+			case Direction.DownRight:
+				nextTileCoordinate.x++;
+				nextTileCoordinate.y--;
+				break;
+			case Direction.Down:
+				nextTileCoordinate.y--;
+				break;
+			case Direction.LeftDown:
+				nextTileCoordinate.y--;
+				nextTileCoordinate.x--;
+				break;
+			case Direction.Left:
+				nextTileCoordinate.x--;
+				break;
+			case Direction.UpLeft:
+				nextTileCoordinate.x--;
+				nextTileCoordinate.y++;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+		}
+
+    return GameManager.Instance.ListTileController.Find(tile => tile.coordinate == nextTileCoordinate);
+
+}
+
    public enum TileState
   {
     None,
     X,
     O
+  }
+
+  public enum Direction
+  {
+    Up, UpRight, Right, Down, DownRight, Left, UpLeft, LeftDown
   }
 
 }
